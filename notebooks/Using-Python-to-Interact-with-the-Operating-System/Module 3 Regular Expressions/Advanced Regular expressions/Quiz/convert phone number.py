d2a1838eb3c5668def9bdf951d7f5ac787018c60
lubnazarine <86013348+lubnazarine@users.noreply.github.com>
2024-01-28 21:03:25 +0000

    regrex

--- notebooks/Using-Python-to-Interact-with-the-Operating-System/Module 3 Regular Expressions/Advanced Regular expressions/Quiz/convert phone number.py
+++ notebooks/Using-Python-to-Interact-with-the-Operating-System/Module 3 Regular Expressions/Advanced Regular expressions/Quiz/convert phone number.py
@@ -1,4 +1,4 @@
-"""The convert_phone_number function checks for a U.S. phone number format: XXX-XXX-XXXX (3 digits followed by a dash, 3 more digits followed by a dash, and 4 digits), and converts it to a more formal format that looks like this: (XXX) XXX-XXXX. Fill in the regular expression to complete this function.
+"""The convert_phone_number function checks for a U.S. phone number format: "XXX-XXX-XXXX" (3 digits followed by a dash, 3 more digits followed by a dash, and 4 digits), and converts it to a more formal format that looks like this: (XXX) XXX-XXXX. Fill in the regular expression to complete this function.
 """
 import re
 def convert_phone_number(phone):
